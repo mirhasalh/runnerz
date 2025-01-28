e8c081ca31c81babc0158373f562098acefc70c4
Irhas <muhammadirhasalh@gmail.com>
2025-01-28 14:28:35 +0700

    feat(runs): add basic crud

---
+++ src/main/java/lol/irhas/runnerz/run/Location.java
@@ -0,0 +1,5 @@
+package lol.irhas.runnerz.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
---
+++ src/main/java/lol/irhas/runnerz/run/Run.java
@@ -0,0 +1,12 @@
+package lol.irhas.runnerz.run;
+
+import java.time.LocalDateTime;
+
+public record Run(
+        Integer id,
+        String title,
+        LocalDateTime startedOn,
+        LocalDateTime endedOn,
+        Integer miles,
+        Location location
+) { }

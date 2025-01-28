d89bf9d2fd07935d4892b657ee5e7073b8d90662
Irhas <muhammadirhasalh@gmail.com>
2025-01-28 14:54:02 +0700

    fix(runs): add validation

--- src/main/java/lol/irhas/runnerz/run/Run.java
+++ src/main/java/lol/irhas/runnerz/run/Run.java
@@ -1,12 +1,23 @@
 package lol.irhas.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
         Integer id,
+        @NotEmpty
         String title,
         LocalDateTime startedOn,
         LocalDateTime endedOn,
+        @Positive
         Integer miles,
         Location location
-) { }
+) {
+    public Run {
+        if (!endedOn.isAfter(startedOn)) {
+            throw new IllegalArgumentException("endedOn must be after startedOn");
+        }
+    }
+}
--- src/main/java/lol/irhas/runnerz/run/RunController.java
+++ src/main/java/lol/irhas/runnerz/run/RunController.java
@@ -1,6 +1,7 @@
 package lol.irhas.runnerz.run;
 
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -26,20 +27,20 @@ public class RunController {
     Run findById(@PathVariable Integer id) {
         Optional<Run> run = runRepository.findById(id);
         if (run.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+            throw new RunNotFoundException();
         }
         return run.get();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@RequestBody Run run) {
+    void create(@Valid @RequestBody Run run) {
         runRepository.create(run);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
         runRepository.update(run, id);
     }
 
